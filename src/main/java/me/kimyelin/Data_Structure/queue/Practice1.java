c14d03508599322be494956bc0f21c8d82091bcd
김예린 <annalin1004@gmail.com>
2024-10-30 15:30:36 +0900

    Practice Queue
     For more information, see Data Structures - Week 4 in README.md.

---
+++ src/main/java/me/kimyelin/Data_Structure/queue/Practice1.java
@@ -0,0 +1,79 @@
+package me.kimyelin.Data_Structure.queue;
+
+import java.util.ArrayList;
+
+import static me.kimyelin.Main.line;
+
+//ArrayList를 이용한 queue 구현
+class MyQueue1{
+    ArrayList list;
+
+    MyQueue1(){
+        this.list = new ArrayList();
+    }
+
+    public boolean isEmpty(){
+        if(this.list.size() == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public void push(int data){
+        this.list.add(data);
+    }
+
+    public Integer pop(){
+        if(this.isEmpty()){
+            System.out.println("list is empty");
+            return null;
+        }
+
+        int data = (int) this.list.get(0);  //generic되어 있지 않으므로
+        this.list.remove(0);
+        return data;
+    }
+
+    public Integer peek(){
+        if(this.isEmpty()){
+            System.out.println("list is empty");
+            return null;
+        }
+
+        return (int) this.list.get(0);
+    }
+
+    public void printQueue(){
+        System.out.println(this.list);
+    }
+}
+
+public class Practice1 {
+    public static void main(String[] args){
+        MyQueue1 myQueue = new MyQueue1();
+        myQueue.push(1);
+        myQueue.push(2);
+        myQueue.push(3);
+        myQueue.push(4);
+        myQueue.push(5);
+        myQueue.printQueue();
+        line();
+
+        System.out.println(myQueue.peek());
+        myQueue.printQueue();
+        line();
+
+        System.out.println(myQueue.pop());
+        myQueue.printQueue();
+        line();
+
+        System.out.println(myQueue.pop());
+        myQueue.printQueue();
+        line();
+
+        System.out.println(myQueue.pop());
+        System.out.println(myQueue.pop());
+        System.out.println(myQueue.pop());
+        myQueue.printQueue();
+    }
+}
---
+++ src/main/java/me/kimyelin/Data_Structure/queue/Practice2.java
@@ -0,0 +1,81 @@
+package me.kimyelin.Data_Structure.queue;
+//Array를 이용한 큐 구현(원형 큐)
+import static me.kimyelin.Main.line;
+
+class MyQueue2{
+    int[] arr;
+    int front = 0;
+    int rear = 0;
+
+    MyQueue2(int size){
+       arr = new int[size+1];
+    }
+
+    public boolean isEmpty(){
+        return this.rear == this.front;
+    }
+
+    public boolean isFull(){
+        return (this.rear +1)%this.arr.length == this.front;
+    }
+
+    public void enqueue(int data){
+        if(this.isFull()){
+            System.out.println("Queue is full");
+            return;
+        }
+
+        this.rear = (this.rear + 1) % this.arr.length;  //원형으로 관리하기 위해
+        this.arr[this.rear] = data;
+    }
+
+    public Integer dequeue(){
+        if(this.isEmpty()){
+            System.out.println("Queue is empty");
+            return null;
+        }
+
+        front = (front + 1) % this.arr.length;
+        return this.arr[front];
+    }
+
+    public void printQueue(){
+        int start = (this.front+1)%this.arr.length;
+        int end = (this.rear+1)% this.arr.length;
+
+        for (int i = start; i != end; i=(i+1)%this.arr.length) {
+            System.out.print(this.arr[i]+ " ");
+        }
+        System.out.println();
+    }
+}
+
+public class Practice2 {
+    public static void main(String[] args){
+        MyQueue1 myQueue = new MyQueue1();
+        myQueue.push(1);
+        myQueue.push(2);
+        myQueue.push(3);
+        myQueue.push(4);
+        myQueue.push(5);
+        myQueue.printQueue();
+        line();
+
+        System.out.println(myQueue.peek());
+        myQueue.printQueue();
+        line();
+
+        System.out.println(myQueue.pop());
+        myQueue.printQueue();
+        line();
+
+        System.out.println(myQueue.pop());
+        myQueue.printQueue();
+        line();
+
+        System.out.println(myQueue.pop());
+        System.out.println(myQueue.pop());
+        System.out.println(myQueue.pop());
+        myQueue.printQueue();
+    }
+}
